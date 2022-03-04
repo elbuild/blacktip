efb773f520f110f35ed710f06355978b24a53b54
Lorenzo <lorenzo.cruccu@elbuild.it>
2022-03-04 16:21:22 +0100

    startofday and endofday

--- src/modules/date.ts
+++ src/modules/date.ts
@@ -7,3 +7,21 @@ export function newDate(time: boolean = false): Date {
 	}
 	return d;
 }
+
+//it returns the start of the day from input date object
+export function startOfDay(d?: Date): Date {
+	if (!d) {
+		d = new Date();
+	}
+	d.setHours(0, 0, 0, 0);
+	return d;
+}
+
+
+export function endOfDay(d?: Date): Date {
+	if (!d) {
+		d = new Date();
+	}
+	d.setHours(23, 59, 59);
+	return d;
+}
--- src/test/date.spec.ts
+++ src/test/date.spec.ts
@@ -1,11 +1,11 @@
-import { newDate } from '../modules/date';
+import { endOfDay, newDate, startOfDay } from '../modules/date';
 
 
 describe('date module', () => {
 
 	const realdate = new Date();
 
-	it('should create a new date', () => {
+	it('should create a new date obj', () => {
 		const value = newDate(false);
 		expect(value.getFullYear()).toBe(realdate.getFullYear());
 		expect(value.getMonth()).toBe(realdate.getMonth());
@@ -16,7 +16,7 @@ describe('date module', () => {
 
 	});
 
-	it('should create a new date with time', () => {
+	it('should create a new date obj with time', () => {
 		const value = newDate(true);
 		expect(value.getFullYear()).toBe(realdate.getFullYear());
 		expect(value.getMonth()).toBe(realdate.getMonth());
@@ -24,6 +24,28 @@ describe('date module', () => {
 		expect(value.getHours()).toBe(realdate.getHours());
 		expect(value.getMinutes()).toBe(realdate.getMinutes());
 		//expect(value.getSeconds()).toBe(0); can't test minutes 
+
+	});
+
+	it('should create a new date obj with 0 hours, 0 minuts and so on ', () => {
+		const value = startOfDay(realdate);
+		expect(value.getFullYear()).toBe(realdate.getFullYear());
+		expect(value.getMonth()).toBe(realdate.getMonth());
+		expect(value.getDay()).toBe(realdate.getDay());
+		expect(value.getHours()).toBe(0);
+		expect(value.getMinutes()).toBe(0);
+		expect(value.getSeconds()).toBe(0);
+
+	});
+
+	it('should create a new date obj with 23 hours, 59 minuts and so on ', () => {
+		const value = endOfDay(realdate);
+		expect(value.getFullYear()).toBe(realdate.getFullYear());
+		expect(value.getMonth()).toBe(realdate.getMonth());
+		expect(value.getDay()).toBe(realdate.getDay());
+		expect(value.getHours()).toBe(23);
+		expect(value.getMinutes()).toBe(59);
+		expect(value.getSeconds()).toBe(59);
 
 	});
 
