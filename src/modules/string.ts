294cbfec93c83b92238b050a32f5559c17d4a5fd
Matteo <matteo.cirri@elbuild.it>
2022-04-22 15:29:32 +0200

    add capitalize isJson

--- src/modules/string.ts
+++ src/modules/string.ts
@@ -1,11 +1,30 @@
 
 export function leftPad(s: string, size: number, char: string): string {
-    let pad = '';
-    while ((pad.length + s.length) < size) pad += char;
-    return pad + s;
+	let pad = '';
+	while ((pad.length + s.length) < size) pad += char;
+	return pad + s;
 }
 
 export function isNotBlank(value: string): boolean {
-    return typeof value !== 'undefined' && value !== null && value.trim().length > 0;
+	return typeof value !== 'undefined' && value !== null && value.trim().length > 0;
 }
 
+export function capitalize(value: string) {
+	return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+}
+
+export function isJson(item: any) {
+	item = typeof item !== 'string' ? JSON.stringify(item) : item;
+
+	try {
+		item = JSON.parse(item);
+	} catch (e) {
+		return false;
+	}
+
+	if (typeof item === 'object' && item !== null) {
+		return true;
+	}
+
+	return false;
+}
--- src/test/string.spec.ts
+++ src/test/string.spec.ts
@@ -1,24 +1,43 @@
-import { isNotBlank, leftPad } from '../modules/string';
+import { isNotBlank, leftPad, capitalize, isJson } from '../modules/string';
 
 describe('string module', () => {
-    it('check empty string', () => {
-        expect(isNotBlank('')).toBe(false);
-    })
+	it('check empty string', () => {
+		expect(isNotBlank('')).toBe(false);
+	})
 
-    it('check not empty string', () => {
-        expect(isNotBlank('ELbuild')).toBe(true);
-    })
+	it('check not empty string', () => {
+		expect(isNotBlank('ELbuild')).toBe(true);
+	})
 
-    it('check pad size = 0', () => {
-        expect(leftPad('1', 0, '0')).toBe('1');
-    })
+	it('check pad size = 0', () => {
+		expect(leftPad('1', 0, '0')).toBe('1');
+	})
 
-    it('pad string with size equals to pad size', () => {
-        expect(leftPad('1', 1, '0')).toBe('1');
-    })
+	it('pad string with size equals to pad size', () => {
+		expect(leftPad('1', 1, '0')).toBe('1');
+	})
 
-    it('add 0 zero to 1 = 01', () => {
-        expect(leftPad('1', 2, '0')).toBe('01');
-    })
+	it('add 0 zero to 1 = 01', () => {
+		expect(leftPad('1', 2, '0')).toBe('01');
+	})
+
+	it('check first letter', () => {
+		expect(capitalize('elbuild')).toBe('Elbuild');
+	})
+	it('check string', () => {
+		expect(capitalize('eLBUILD')).toBe('Elbuild');
+	})
+
+	it('check json', () => {
+		expect(isJson('{"name":"Mario","surname": "Rossi","active": true,"favoriteNumber": 42,"birthday": {"day": 1,"month": 1,"year": 2000},"languages": ["it", "en"]}')).toBe(true);
+	})
+
+	it('check not json', () => {
+		expect(isJson('{4:"Mario",surname: "Rossi","active": true,"favoriteNumber": 42,"birthday": {"day": 1,("month"): 1,"year": 2000}, "languages": ["it", "en"]}')).toBe(false);
+	})
+
+	it('check number json', () => {
+		expect(isJson(5)).toBe(false);
+	})
 })
 
